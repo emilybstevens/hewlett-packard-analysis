6c0ea02680ed1eb00a763f3b333e3e20239c4c41
emilybstevens <98931234+emilybstevens@users.noreply.github.com>
2022-04-10 15:04:21 -0500

    finalized challenge code

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -38,3 +38,44 @@ GROUP BY title
 ORDER BY COUNT(title) DESC;
 
 
+-- DELIVERABLE 2 ************************************************
+
+SELECT DISTINCT ON(e.emp_no)e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date, 
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+	on(e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+	on(e.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY emp_no ASC;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
